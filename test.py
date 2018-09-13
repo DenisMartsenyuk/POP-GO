41bc7f5980df340fa1bd8e0437106cac7d2f6c37
pprettysimpple <andrianov.kirill2010@yandex.ru>
2018-09-13 20:36:25 +0300

    test game

---
+++ test.py
@@ -0,0 +1,15 @@
+oppenent = 13476138617 #smth
+
+def send_to(id, x, y):
+    #send x, y, to opponent`s socket
+    pass
+
+while 1:
+    x, y
+    listen(x, y)
+    do_turn(x, y)
+    #read your x, y
+    send_to(oppenent, x, y)
+    pass
+
+
