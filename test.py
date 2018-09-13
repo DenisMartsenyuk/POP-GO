fe6bb03d0835aa17e69a14cdd227230b145933cc
pprettysimpple <andrianov.kirill2010@yandex.ru>
2018-09-13 20:43:14 +0300

    test game

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 oppenent = 13476138617 #smth
 
+def listen():
+    #return x, y from socket
+    pass
+
 def send_to(id, x, y):
     #send x, y, to opponent`s socket
     pass
 
 while 1:
-    x, y
-    listen(x, y)
+    x, y = listen(x, y)
     do_turn(x, y)
     #read your x, y
+    do_turn(x, y)
     send_to(oppenent, x, y)
     pass
 
