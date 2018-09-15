e74d0987a6639a4f4637a628c309d7f8f6217dcd
kirill <sizovki@yandex.ru>
2018-09-15 12:02:27 +0300

    almost working code

--- inside.py
+++ inside.py
@@ -1,8 +1,45 @@
-class session:
-    N, turn = 32, 1
-    matrix = [0] * (N * N)
-    def do_move(x, y):
-        add_point(x, y, turn)
-        hull = find_hull(x, y)
-        turn *= -1
-        return hull
+class Point:
+    def __init__(self):
+        self.is_active = True
+        self.color = 0
+
+
+class Logic:
+    def __init__(self, size):
+        self.n = size
+        self.table = [[Point()] * size for i in range(size)]
+        self.count1 = 0
+        self.count2 = 0
+        self.cur_player = 1
+
+    def out_of_board(self, x, y):
+        return x < 0 or y < 0 or x >= self.n or y >= self.n
+
+    # возвращает 0 или 1, 0 если не пришли к границе, 1 в противном случае
+    def dfs(self, used, x, y, color, hull, inside):
+        if self.out_of_board(x, y):
+            return True
+        if used[x][y]:
+            return False
+        if self.table[x][y].is_active and self.table[x][y].color == -color:
+            hull.append([x, y])
+            return False
+        used[x][y] = 1
+        inside.append([x, y])
+        ans = 0
+        ans = ans or self.dfs(used, x - 1, y, color, hull, inside)
+        ans = ans or self.dfs(used, x + 1, y, color, hull, inside)
+        ans = ans or self.dfs(used, x, y - 1, color, hull, inside)
+        ans = ans or self.dfs(used, x, y + 1, color, hull, inside)
+        return ans
+
+    def update(self, x, y):
+        used = [[False] * self.n for i in range(self.n)]
+        hull, inside = [], []
+        if self.dfs(used, x, y, self.table[x][y].color, hull, inside):
+
+
+    def do_turn(self, x, y):
+        self.table[x][y].color = self.cur_player
+        self.cur_player *= -1
+        #update(x, y)
