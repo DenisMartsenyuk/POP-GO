5dc79359f4cde2f5b34056f46a0c73ae63e2cad2
pprettysimpple <andrianov.kirill2010@yandex.ru>
2018-09-15 09:52:55 +0300

    inside

--- inside.py
+++ inside.py
@@ -1,2 +1,8 @@
-import tkinter
-import
+class session:
+    N, turn = 32, 1
+    matrix = [0] * (N * N)
+    def do_move(x, y):
+        add_point(x, y, turn)
+        hull = find_hull(x, y)
+        turn *= -1
+        return hull
